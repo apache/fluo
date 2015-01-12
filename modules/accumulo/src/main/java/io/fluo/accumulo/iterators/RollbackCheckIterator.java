16c11674be7142a9f0aaf12899038197495b66bb
Keith Turner <kturner@apache.org>
2015-01-12 16:40:15 -0500

    fixes #397 assign return val from maximizeStartKeyTimeStamp()

--- modules/accumulo/src/main/java/io/fluo/accumulo/iterators/RollbackCheckIterator.java
+++ modules/accumulo/src/main/java/io/fluo/accumulo/iterators/RollbackCheckIterator.java
@@ -69,7 +69,7 @@ public class RollbackCheckIterator implements SortedKeyValueIterator<Key,Value> 
   
   @Override
   public void seek(Range range, Collection<ByteSequence> columnFamilies, boolean inclusive) throws IOException {
-    IteratorUtil.maximizeStartKeyTimeStamp(range);
+    range = IteratorUtil.maximizeStartKeyTimeStamp(range);
 
     source.seek(range, columnFamilies, inclusive);
     
