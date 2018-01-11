53c7b73ec564d678597f0a72e7363b1c2ac98c09
Mike Walch <mwalch@apache.org>
2018-01-11 11:21:40 -0500

    Changes from review

--- modules/command/src/main/java/org/apache/fluo/command/FluoRemove.java
+++ modules/command/src/main/java/org/apache/fluo/command/FluoRemove.java
@@ -15,71 +15,30 @@
 
 package org.apache.fluo.command;
 
-import java.io.File;
-
-import com.beust.jcommander.Parameter;
-import com.google.common.base.Preconditions;
-import org.apache.fluo.api.client.FluoAdmin;
 import org.apache.fluo.api.config.FluoConfiguration;
 import org.apache.fluo.core.client.FluoAdminImpl;
 
 public class FluoRemove {
 
-  public static class RemoveOptions extends CommonOpts {
-
-    @Parameter(names = "-p", required = true, description = "Path to application properties file")
-    private String appPropsPath;
-
-    String getAppPropsPath() {
-      return appPropsPath;
-    }
-
-    public static RemoveOptions parse(String[] args) {
-      RemoveOptions opts = new RemoveOptions();
-      parse("fluo remove", opts, args);
-      return opts;
-    }
-  }
-
   public static void main(String[] args) {
-    RemoveOptions opts = RemoveOptions.parse(args);
-    File applicationPropsFile = new File(opts.getAppPropsPath());
-    Preconditions.checkArgument(applicationPropsFile.exists(),
-        opts.getAppPropsPath() + " does not exist");
+
+    CommonOpts opts = CommonOpts.parse("fluo remove", args);
 
     FluoConfiguration config = CommandUtil.resolveFluoConfig();
-    config.load(applicationPropsFile);
     config.setApplicationName(opts.getApplicationName());
     opts.overrideFluoConfig(config);
-
-    if (!config.hasRequiredAdminProps()) {
-      System.err.println("Error - Required properties are not set in " + opts.getAppPropsPath());
-      System.exit(-1);
-    }
-    try {
-      config.validate();
-    } catch (Exception e) {
-      System.err.println("Error - Invalid configuration due to " + e.getMessage());
-      System.exit(-1);
-    }
+    config = FluoAdminImpl.mergeZookeeperConfig(config);
 
     try (FluoAdminImpl admin = new FluoAdminImpl(config)) {
-
       if (admin.applicationRunning()) {
         System.err.println("Error - The Fluo '" + config.getApplicationName() + "' application"
             + " is already running and must be stopped before running 'fluo remove'. "
             + " Aborted remove.");
         System.exit(-1);
       }
-
-      FluoAdmin.InitializationOptions initOpts = new FluoAdmin.InitializationOptions();
-      initOpts.setClearZookeeper(true).setClearTable(true);
-      initOpts.setClearTable(true);
-
-      System.out.println("Removing Fluo '" + config.getApplicationName() + "' application using "
-          + opts.getAppPropsPath());
+      System.out.println("Removing Fluo '" + config.getApplicationName());
       try {
-        admin.remove(initOpts);
+        admin.remove();
       } catch (Exception e) {
         System.out.println("Remove failed due to the following exception:");
         e.printStackTrace();
