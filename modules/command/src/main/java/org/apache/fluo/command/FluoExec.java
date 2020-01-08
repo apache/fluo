5181daa8840c528646845da973d95afa93ee861d
Joe Koshakow <koshy44@gmail.com>
2020-01-07 19:42:45 -0500

    Fix Type Issue with FluoExec

--- modules/command/src/main/java/org/apache/fluo/command/FluoExec.java
+++ modules/command/src/main/java/org/apache/fluo/command/FluoExec.java
@@ -72,7 +72,8 @@ public class FluoExec extends BaseCommand implements FluoCommand {
       Guice.createInjector(new FluoConfigModule(clazz, fluoConfig));
 
       Method method = clazz.getMethod("main", String[].class);
-      method.invoke(null, args.subList(2, args.size()));
+      List<String> execArgs = args.subList(2, args.size());
+      method.invoke(null, (Object) execArgs.toArray(new String[execArgs.size()]));
     } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
       throw new FluoCommandException(String.format("Class %s must have a main method", className),
           e);
